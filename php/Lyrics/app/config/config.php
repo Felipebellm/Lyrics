1d9cb5ec515609ac6afbc8fc386e6b5db24eb19f
Felipe Bell Marques <felipe.bell@smc.priv>
2025-04-16 13:23:07 -0300

    update

---
+++ php/Lyrics/app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+    // APP Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // URL Root
+    define('URLROOT', 'http://localhost:8000/Lyrics');
+
+    // Site Name
+    define('SITENAME', 'Lyrics');
+
+    // DB Params
+    define('DB_HOST','db');
+    define('DB_USER','root');
+    define('DB_PASS','pass@122546');
+    define('DB_NAME','db_lyrics');
